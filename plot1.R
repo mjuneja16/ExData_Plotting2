bab99eb32ce0b40e058ae51d47f94ad011c9c279
Mohit Juneja <mohit.juneja16@gmail.com>
2014-06-22 23:03:18 +0530

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,11 +5,11 @@ if(!file.exists("./Source_Classification_Code.rds") & !file.exists("./summarySCC
   download.file(fileUrl,destfile="NEI_data.zip")
   unzip("NEI_data.zip")
 }
-##read the first source file in the data frame NEI
+##read the contents of the first source file in the data set NEI
 NEI <- readRDS("summarySCC_PM25.rds")
-##read the other source file in the data frame SCC
+##read the contents of the other source file in the data set SCC
 SCC <- readRDS("Source_Classification_Code.rds")
-##aggregate the 'Emissions' in the United States by year
+##aggregate the 'Emissions' (in the United States) by year
 Emissions<-aggregate(NEI[,"Emissions"],by=list(NEI$year), FUN = sum)
 ##remane the columns names of the data set 'Emissions' as 'Year' and 'Total.Emissions'
 colnames(Emissions)<-c("Year","Total.Emissions")
@@ -19,7 +19,7 @@ Emissions<-transform(Emissions,Year=factor(Year))
 ##open the PNG file device in the working directory
 png(filename="plot1.png",width=480,height=480,units="px")
 par(mar=c(5,5,4,2))
-##plot the 'Total PM2.5 Emissions in the United States' across the years 1999-2008 (no plot appears on the screen)
+##plot the 'Total PM2.5 Emissions in the United States' across the years 1999-2008 using the base plotting system
 barplot(height=Emissions$Total.Emissions,names.arg=Emissions$Year,
      xlab="Year",ylab=expression('Emissions of PM'[2.5]*' (in kilotons)'), 
      main=expression('Total Emissions of PM'[2.5]*' in the United States'), col = "red")
